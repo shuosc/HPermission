309d0ac06235f6e104a67e887cd66bde261cf693
longfangsong <longfangsong@icloud.com>
2020-09-13 19:29:44 +0800

    For debug

--- src/handler.rs
+++ src/handler.rs
@@ -2,30 +2,46 @@ use actix_web::{HttpRequest, Responder, HttpResponse};
 use crate::token::get_username;
 use crate::CONFIG;
 use actix_http::http::Method;
+use actix_web::http::Uri;
 
 pub async fn ping(_: HttpRequest) -> impl Responder {
     "pong"
 }
 
 pub async fn check(request: HttpRequest) -> impl Responder {
-    for header in request.headers() {
-        println!("{:?}", header);
-    }
+    let forwarded_uri = request.headers().get("x-forwarded-uri")
+        .and_then(|it| it.to_str().ok())
+        .and_then(|it| it.parse::<Uri>().ok());
+    let (path, query) = if let Some(uri) = &forwarded_uri {
+        (uri.path(), uri.query().unwrap_or(""))
+    } else {
+        (request.path(), request.query_string())
+    };
     let username = request.headers().get("Authorization")
         .and_then(|it| it.to_str().ok())
         .map(|it| &it["Bearer ".len()..])
         .and_then(get_username);
-    let write = match request.method() {
-        &Method::POST | &Method::PUT | &Method::DELETE => true,
-        &Method::GET => false,
-        _ => unimplemented!()
+    let forwarded_method = request.headers().get("x-forwarded-method")
+        .and_then(|it| it.to_str().ok());
+    let write = if let Some(forwarded_method) = forwarded_method {
+        match forwarded_method {
+            "POST" | "PUT" | "DELETE" => true,
+            "GET" => false,
+            _ => return HttpResponse::UnprocessableEntity()
+        }
+    } else {
+        match request.method() {
+            &Method::POST | &Method::PUT | &Method::DELETE => true,
+            &Method::GET => false,
+            _ => return HttpResponse::UnprocessableEntity()
+        }
     };
-    let passed = CONFIG.check(username.as_ref(), request.path(), request.query_string(), write);
+    let passed = CONFIG.check(username.as_ref(), path, query, write);
     if passed {
-        info!("Allow {} to visit {}", username.unwrap_or("anonymous user".to_string()), request.path());
+        info!("Allow {} to {} {}", username.unwrap_or("anonymous user".to_string()), if write { "write" } else { "read" }, path);
         HttpResponse::Ok()
     } else {
-        info!("Disallow {} to visit {}", username.unwrap_or("anonymous user".to_string()), request.path());
+        info!("Disallow {} to {} {}", username.unwrap_or("anonymous user".to_string()), if write { "write" } else { "read" }, path);
         HttpResponse::Unauthorized()
     }
 }
